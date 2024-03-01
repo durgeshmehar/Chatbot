b6cc1b66666c07a6eecd74d8ee526ce121841e66
Durgesh Mehar <durgeshmehar2002@gmail.com>
2024-03-01 22:40:07 +0530

    added cors in backend

--- chatbot_frontend/next.config.mjs
+++ chatbot_frontend/next.config.mjs
@@ -2,18 +2,6 @@
 const nextConfig = {
     async rewrites() {
     return [
-      {
-        source: '/users/signin',
-        destination: 'http://localhost:8000/users/signin',
-      },
-      {
-        source: '/users/signup',
-        destination: 'http://localhost:8000/users/signup',
-      },
-      {
-        source: '/chats/chat',
-        destination: 'http://localhost:8000/chats/chat',
-      },
     ]
   },
 };
