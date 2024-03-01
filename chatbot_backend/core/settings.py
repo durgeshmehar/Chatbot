bef1e87ed628b11213c6e6c41ffa06053c4ad0af
Durgesh Mehar <durgeshmehar2002@gmail.com>
2024-03-01 22:05:54 +0530

    add os.getenv

--- chatbot_backend/core/settings.py
+++ chatbot_backend/core/settings.py
@@ -2,7 +2,7 @@ import tomllib
 from functools import cached_property
 from pathlib import Path
 from typing import Literal
-
+import os
 from pydantic import AnyHttpUrl, EmailStr, PostgresDsn, computed_field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
@@ -11,17 +11,17 @@ PROJECT_DIR = Path(__file__).parent.parent.parent
 
 class Settings(BaseSettings):
     # CORE SETTINGS
-    SECRET_KEY: str = "DVnFmhwvjEhJZpuhndxjhlezxQPJmBIIkMDEmFREWQADPcUnrG"
+    SECRET_KEY: str = os.getenv("SECRET_KEY") 
     ENVIRONMENT: Literal["DEV", "PYTEST", "STG", "PRD"] = "DEV"
     SECURITY_BCRYPT_ROUNDS: int = 12
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 11520  # 8 days
     REFRESH_TOKEN_EXPIRE_MINUTES: int = 40320  # 28 days
     BACKEND_CORS_ORIGINS: list[AnyHttpUrl] = []
     ALLOWED_HOSTS: list[str] = ["*"]
-    OPENAI_API_KEY: str = None
+    OPENAI_API_KEY: str =  os.getenv("OPENAI_API_KEY") 
 
     model_config = SettingsConfigDict(
-        env_file=f"{PROJECT_DIR}/chatbot_backend/.env", case_sensitive=False
+        env_file=f"{PROJECT_DIR}/.env", case_sensitive=False
     )
 
 settings: Settings = Settings()
