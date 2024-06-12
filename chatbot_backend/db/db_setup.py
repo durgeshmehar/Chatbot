ce9f5ffc60387f6e9d58ee824a8f5b77f5bfa97f
Durgesh Mehar <durgeshmehar2002@gmail.com>
2024-06-12 19:14:25 +0530

    add file in temp file 1

--- chatbot_backend/db/db_setup.py
+++ chatbot_backend/db/db_setup.py
@@ -5,8 +5,9 @@ from core.settings import settings
 
 # Elephantsql
 # DATABASE ="postgresql://vdpjdnij:j5OFZu7g9FyrSNnoNGQuDhZick1_CgW8@rain.db.elephantsql.com/vdpjdnij"
-DATABASE="postgresql://vdpjdnij:j5OFZu7g9FyrSNnoNGQuDhZick1_CgW8@rain.db.elephantsql.com/vdpjdnij"
 # DATABASE = "postgresql://postgres:postgres@localhost:5432/chatbot"
+# DATABASE="postgresql://vdpjdnij:j5OFZu7g9FyrSNnoNGQuDhZick1_CgW8@rain.db.elephantsql.com/vdpjdnij"
+DATABASE="postgresql://niwncmfs:hRe3VBBAogOX6chv9hUZt_Vr1Vji97ja@tiny.db.elephantsql.com/niwncmfs"
 
 engine = create_engine(DATABASE )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
